1864538868f0b5f43a3ee3686348da15308e0f00
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-04-13 17:37:24 -0400

    Add playEffect for healpool
     public void playEffect(Player player, Location location){
            player.spawnParticle(Particle.VILLAGER_HAPPY, location, 1);
        }

--- versionsupport_v1_18_R2/src/main/java/com/andrei1058/bedwars/support/version/v1_18_R2/v1_18_R2.java
+++ versionsupport_v1_18_R2/src/main/java/com/andrei1058/bedwars/support/version/v1_18_R2/v1_18_R2.java
@@ -714,4 +714,9 @@ public class v1_18_R2 extends VersionSupport {
     public void clearArrowsFromPlayerBody(Player player) {
         ((CraftLivingEntity)player).getHandle().ai().b(new DataWatcherObject<>(12, DataWatcherRegistry.b),-1);
     }
+    
+    @Override
+    public void playEffect(Player player, Location location){
+        player.spawnParticle(Particle.VILLAGER_HAPPY, location, 1);
+    }
 }
