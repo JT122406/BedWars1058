f394072cba0e4aa1ac0a25544075b141ca3c19b3
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-08-16 11:47:42 -0400

    Revert "Move from PlayerPickUpItem to EntityPickUpItem"
    This reverts commit 6e84905cdfa7bc878e1820f91f43773e92f1fce4.

--- versionsupport_common/src/main/java/com/andrei1058/bedwars/listeners/PlayerDropPick_1_11Minus.java
+++ versionsupport_common/src/main/java/com/andrei1058/bedwars/listeners/PlayerDropPick_1_11Minus.java
@@ -27,11 +27,10 @@ import com.andrei1058.bedwars.api.events.player.PlayerGeneratorCollectEvent;
 import com.andrei1058.bedwars.api.server.ServerType;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -46,68 +45,62 @@ public class PlayerDropPick_1_11Minus implements Listener {
 
     /* This Class is used for versions from 1.8.8 to 1.11 included */
 
-
+    @SuppressWarnings("deprecation")
     @EventHandler
-    public void onPickup(EntityPickupItemEvent e) {
-        if (e instanceof Player)
-        {
-            Player p = (Player) e.getEntity();
-            if (api.getServerType() != ServerType.BUNGEE) {
-                //noinspection ConstantConditions
-                if (p.getLocation().getWorld().getName().equalsIgnoreCase(api.getLobbyWorld())) {
-                    e.setCancelled(true);
-                    return;
-                }
-            }
-
-            IArena a = api.getArenaUtil().getArenaByPlayer(e.getPlayer());
-            if (a == null) return;
-
-            if (!a.isPlayer(p)) {
+    public void onPickup(PlayerPickupItemEvent e) {
+        if (api.getServerType() != ServerType.BUNGEE) {
+            //noinspection ConstantConditions
+            if (e.getPlayer().getLocation().getWorld().getName().equalsIgnoreCase(api.getLobbyWorld())) {
                 e.setCancelled(true);
                 return;
             }
+        }
 
-            if (a.getStatus() != GameState.playing) {
-                e.setCancelled(true);
-                return;
-            }
+        IArena a = api.getArenaUtil().getArenaByPlayer(e.getPlayer());
+        if (a == null) return;
 
-            if (a.getRespawnSessions().containsKey(p)) {
-                e.setCancelled(true);
-                return;
-            }
+        if (!a.isPlayer(e.getPlayer())) {
+            e.setCancelled(true);
+            return;
+        }
 
-            if (e.getItem().getItemStack().getType() == Material.ARROW){
-                e.getItem().setItemStack(api.getVersionSupport().createItemStack(e.getItem().getItemStack().getType().toString(), e.getItem().getItemStack().getAmount(), (short) 0));
-                return;
-            }
+        if (a.getStatus() != GameState.playing) {
+            e.setCancelled(true);
+            return;
+        }
 
-            if (e.getItem().getItemStack().getType().toString().equals("BED")) {
-                e.setCancelled(true);
-                e.getItem().remove();
-            } else if (e.getItem().getItemStack().hasItemMeta()) {
-                //noinspection ConstantConditions
-                if (e.getItem().getItemStack().getItemMeta().hasDisplayName()) {
-                    if (e.getItem().getItemStack().getItemMeta().getDisplayName().contains("custom")) {
-                        Material material = e.getItem().getItemStack().getType();
-                        ItemMeta itemMeta = new ItemStack(material).getItemMeta();
+        if (a.getRespawnSessions().containsKey(e.getPlayer())) {
+            e.setCancelled(true);
+            return;
+        }
 
-                        //Call ore pick up event
-                        PlayerGeneratorCollectEvent event = new PlayerGeneratorCollectEvent(p, e.getItem(), a);
-                        Bukkit.getPluginManager().callEvent(event);
-                        if (event.isCancelled()){
-                            e.setCancelled(true);
-                        } else {
-                            e.getItem().getItemStack().setItemMeta(itemMeta);
-                        }
+        if (e.getItem().getItemStack().getType() == Material.ARROW){
+            e.getItem().setItemStack(api.getVersionSupport().createItemStack(e.getItem().getItemStack().getType().toString(), e.getItem().getItemStack().getAmount(), (short) 0));
+            return;
+        }
+
+        if (e.getItem().getItemStack().getType().toString().equals("BED")) {
+            e.setCancelled(true);
+            e.getItem().remove();
+        } else if (e.getItem().getItemStack().hasItemMeta()) {
+            //noinspection ConstantConditions
+            if (e.getItem().getItemStack().getItemMeta().hasDisplayName()) {
+                if (e.getItem().getItemStack().getItemMeta().getDisplayName().contains("custom")) {
+                    Material material = e.getItem().getItemStack().getType();
+                    ItemMeta itemMeta = new ItemStack(material).getItemMeta();
+
+                    //Call ore pick up event
+                    PlayerGeneratorCollectEvent event = new PlayerGeneratorCollectEvent(e.getPlayer(), e.getItem(), a);
+                    Bukkit.getPluginManager().callEvent(event);
+                    if (event.isCancelled()){
+                        e.setCancelled(true);
+                    } else {
+                        e.getItem().getItemStack().setItemMeta(itemMeta);
                     }
                 }
             }
         }
     }
-
-
 
     @EventHandler
     public void onDrop(PlayerDropItemEvent e) {
